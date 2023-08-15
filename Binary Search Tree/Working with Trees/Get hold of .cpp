8fa8732efc36c9c5dfa2daf0e50c4ed728772a2b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-15 19:19:07 +0800

    Committed on or around 2023/08/15

--- Binary Search Tree/Working with Trees/Get hold of .cpp
+++ Binary Search Tree/Working with Trees/Get hold of .cpp
@@ -5,7 +5,8 @@ using namespace std;
 class binaryTree_Node {
 
 	string data;
-
+	
+	binaryTree_Node *left, *right;
 
 
 	binaryTree_Node(string Data, binaryTree_Node *left, binaryTree_Node *right) {
